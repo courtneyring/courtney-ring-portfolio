adabea877ab539bbea23fd1e4b5c4e8c4f65d4fa
Courtney Ring <courtneyring3@gmail.com>
2023-08-31 17:25:33 -0400

    restore tiles

--- src/components/ImageGrid.svelte
+++ src/components/ImageGrid.svelte
@@ -8,7 +8,7 @@
       <div class="grid__tile-text">{tile.title}</div>
       <div
         class="grid__tile-image"
-        style="background-image: url('{tile.image}')"
+        style="background-image: url(/work/{tile.id}/tile.png)"
       />
     </div>
   {/each}
@@ -23,7 +23,12 @@
 
   .grid__tile {
     position: relative;
-    background-color: green;
+    align-items: center;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    width: 100%;
 
     &::before {
       content: '';
@@ -31,11 +36,56 @@
       padding-bottom: 100%;
     }
 
-    & > :first-child {
+    // & > :first-child {
+    //   position: absolute;
+    //   top: 0;
+    //   left: 0;
+    //   height: 100%;
+    // }
+  }
+
+  .grid__tile-text {
+    color: white;
+    font-size: 20px;
+    opacity: 0;
+    max-width: 80%;
+    position: absolute;
+    text-align: center;
+    z-index: 3;
+
+    transition: all 1s ease;
+  }
+  .grid__tile-image {
+    background-size: cover;
+    background-position: center;
+    height: 100%;
+    position: relative;
+    width: 100%;
+
+    transition: all 0.5s ease;
+
+    &::before {
       position: absolute;
-      top: 0;
       left: 0;
+      top: 0;
       height: 100%;
+      width: 100%;
+      display: block;
+      content: ' ';
+      background: #1c1d25;
+      opacity: 0;
+      z-index: 2;
+      transition: all 0.5s ease;
+    }
+  }
+
+  .grid__tile:hover {
+    & > .grid__tile-text {
+      opacity: 1;
+    }
+
+    & > .grid__tile-image::before {
+      opacity: 0.8;
     }
   }
 

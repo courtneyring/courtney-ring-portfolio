9425eea81eac6c05b8c0da919078ca3ad422af6d
Courtney Ring <courtneyring3@gmail.com>
2024-04-21 17:11:36 -0400

    add nav hide/show fn

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,35 +1,51 @@
 <script>
-	import { page } from '$app/stores';
-  import logo from '$lib/images/final-filled-edit.svg'
+  import { onDestroy, onMount } from 'svelte';
+  import { browser } from '$app/environment';
+  import { page } from '$app/stores';
+  import logo from '$lib/images/final-filled-edit.svg';
+
+  let lastScrollY = browser && window.scrollY;
+  $: direction = 'up';
+
+  const updateScrollDirection = () => {
+    const scrollY = window.scrollY;
+    if (scrollY - lastScrollY !== 0) {
+      direction = scrollY > lastScrollY ? 'down' : 'up';
+    }
+    lastScrollY = scrollY > 0 ? scrollY : 0;
+  };
+
+  onMount(() => {
+    browser && window.addEventListener('scroll', updateScrollDirection);
+  });
+
+  onDestroy(() => {
+    browser && window.removeEventListener('scroll', updateScrollDirection);
+  });
 </script>
 
-<header>
-	<a href='/' class='logo'>
-		<img src={logo} />
+<header class={direction == 'down' && 'hide'}>
+  <a href="/" class="logo">
+    <img src={logo} />
   </a>
 
-	<nav>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/work">Work</a>
-			</li>
-			<!-- <li aria-current={$page.url.pathname.startsWith('/work') ? 'page' : undefined}>
+  <nav>
+    <ul>
+      <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+        <a href="/about">About</a>
+      </li>
+      <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+        <a href="/work">Work</a>
+      </li>
+      <!-- <li aria-current={$page.url.pathname.startsWith('/work') ? 'page' : undefined}>
 				<a href="/work">Contact</a>
 			</li> -->
-		</ul>
-
-	</nav>
-
-
+    </ul>
+  </nav>
 </header>
 
-<style lang='scss'>
-
-
-	header {
+<style lang="scss">
+  header {
     align-items: center;
     background-color: transparent;
     display: flex;
@@ -40,13 +56,16 @@
     top: 0;
     width: 100%;
     z-index: 4;
-  }
+    transition: 0.3s ease-in-out margin-top;
 
+    &.hide {
+      margin-top: -80px;
+    }
+  }
 
   nav {
     padding: 10px 30px;
   }
-
 
   nav > ul {
     display: flex;
@@ -70,7 +89,8 @@
   img {
     height: 100%;
     width: auto;
-    filter: invert(100%) sepia(63%) saturate(2%) hue-rotate(278deg) brightness(111%) contrast(101%);
+    filter: invert(100%) sepia(63%) saturate(2%) hue-rotate(278deg)
+      brightness(111%) contrast(101%);
   }
 
   // .corner {
